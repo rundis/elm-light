34adbf125422a4a63e30051f2be024affeae066e
rundis <mrundberget@hotmail.com>
2016-05-09 00:27:23 +0200

    Upping to 0.3.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject elm-light "0.3.6"
+(defproject elm-light "0.3.7"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [quiescent "0.1.4"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]])
