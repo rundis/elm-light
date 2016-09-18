e9dbedb1dfcc73857d2d031b9f2a83665a1675c1
Magnus Rundberget <mrundberget@hotmail.com>
2016-09-19 00:04:59 +0200

    Bumping to v0.4.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject elm-light "0.4.1"
+(defproject elm-light "0.4.2"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [quiescent "0.1.4"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]])
