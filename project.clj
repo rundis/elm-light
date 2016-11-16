ffa94729f9dfb0076384245b396ddc6aa13ad973
Magnus Rundberget <mrundberget@hotmail.com>
2016-11-16 01:38:05 +0100

    Release 0.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject elm-light "0.4.2"
+(defproject elm-light "0.5.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [quiescent "0.1.4"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]])
